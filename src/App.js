0cfd435b9a2a6fc779dc19954c16467df49d0111
davidpitkanen <d.t.pitkanen@gmail.com>
2022-02-09 17:53:20 -0500

    moved about page and home page to their own files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from './components/Home/HomePage';
+import About from './components/About/AboutPage';
 
 function App() {
   return (
@@ -13,37 +15,5 @@ function App() {
     </div>
   );
 }
-
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
-
 
 export default App;
